b5a9e2cfe8285afd702addf58e9991df0c5d63e5
Jatin Prakash <jatin.prakash.cseaiml.2022@miet.ac.in>
2025-04-25 21:49:32 +0530

    feat(auth): implement user authentication interface components with responsive design, form validation and cross-page navigation

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,55 @@
+"use client"
+import Link from "next/link"
+import React from "react"
+import { useRouter } from "next/navigation"
+// import { axios } from "axios"
+
 export default function LoginPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    // username: "",
+  })
+
+  const onLogin = async () => {}
   return (
-    <div className="flex border-2 border-amber-500 justify-center">
-      <h1 className=" text-white text-2xl ">Login</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-3xl font-bold">LOGIN PAGE</h1>
+      <hr />
+
+      <label htmlFor="email">Email</label>
+      <input
+        className="h-8 w-60 rounded-lg p-2 bg-purple-400 text-black my-1.5"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => {
+          setUser({ ...user, email: e.target.value })
+        }}
+        placeholder="email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="h-8 w-60 rounded-lg p-2 bg-purple-400 text-black my-1.5"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => {
+          setUser({ ...user, password: e.target.value })
+        }}
+        placeholder="password"
+      />
+      <button
+        onClick={onLogin}
+        className=" focus:border-white m-1.5 h-8 w-60  rounded-lg bg-gray-500 hover:bg-gray-800 text-shadow-white"
+      >
+        Submit
+      </button>
+      <Link href="/signup">
+        <button className=" focus:border-white m-1.5 h-8 w-60  rounded-lg bg-green-300 hover:bg-green-800 hover:text-white text-black">
+          Visit Signup
+        </button>
+      </Link>
     </div>
   )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,65 @@
+"use client"
+import Link from "next/link"
+import React from "react"
+import { useRouter } from "next/navigation"
+// import { axios } from "axios"
+
 export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  })
+
+  const onSignup = async () => {}
   return (
-    <div className="flex justify-center">
-      <h1 className="text-2xl">SignUp</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-3xl font-bold">SIGNUP PAGE</h1>
+      <hr />
+      <label htmlFor="username">Username</label>
+      <input
+        className="h-8 w-60 rounded-lg p-2 bg-amber-300 text-black my-1.5"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => {
+          setUser({ ...user, username: e.target.value })
+        }}
+        placeholder="username"
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        className="h-8 w-60 rounded-lg p-2 bg-amber-300 text-black my-1.5"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => {
+          setUser({ ...user, email: e.target.value })
+        }}
+        placeholder="email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="h-8 w-60 rounded-lg p-2 bg-amber-300 text-black my-1.5"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => {
+          setUser({ ...user, password: e.target.value })
+        }}
+        placeholder="password"
+      />
+      <button
+        onClick={onSignup}
+        className=" focus:border-white m-1.5 h-8 w-60  rounded-lg bg-amber-600 hover:bg-amber-800 text-amber-200"
+      >
+        Submit
+      </button>
+      <Link href="/login">
+        <button className=" focus:border-white m-1.5 h-8 w-60  rounded-lg bg-red-600 hover:bg-red-700 text-amber-200">
+          Visit Login
+        </button>
+      </Link>
     </div>
   )
 }
