75daf4b4281b8bb76a6769b244ec49676aa0ea4d
Jatin Prakash <jatin.prakash.cseaiml.2022@miet.ac.in>
2025-04-30 12:51:03 +0530

    chore: update .gitignore to exclude environment files

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,44 @@
 "use client"
 import Link from "next/link"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
-// import { axios } from "axios"
+import toast from "react-hot-toast"
+import axios from "axios"
 
 export default function LoginPage() {
+  const router = useRouter()
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     // username: "",
   })
+  const [buttonDisabled, setButtonDisabled] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-  const onLogin = async () => {}
+  const onLogin = async () => {
+    try {
+      setLoading(true)
+      const response = await axios.post("api/users/login", user)
+      console.log("Login successful", response.data)
+      toast.success("Login successful")
+      router.push("/profile")
+    } catch (error: any) {
+      console.log("Login Failed", error.message)
+      toast.error(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false)
+    } else {
+      setButtonDisabled(true)
+    }
+  }, [user])
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-3xl font-bold">LOGIN PAGE</h1>
+      <h1 className="text-3xl font-bold">{loading ? "processing" : "Login"}</h1>
       <hr />
 
       <label htmlFor="email">Email</label>
@@ -43,7 +67,7 @@ export default function LoginPage() {
         onClick={onLogin}
         className=" focus:border-white m-1.5 h-8 w-60  rounded-lg bg-gray-500 hover:bg-gray-800 text-shadow-white"
       >
-        Submit
+        {buttonDisabled ? "Fill in details" : "Login"}
       </button>
       <Link href="/signup">
         <button className=" focus:border-white m-1.5 h-8 w-60  rounded-lg bg-green-300 hover:bg-green-800 hover:text-white text-black">
